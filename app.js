f96cc0dcbf889d2bddebac667a517e128f5860c1
Clifforddd <90348904+Clifforddd@users.noreply.github.com>
2022-05-25 17:46:56 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+const div = document.querySelector(".main-generator");
+const memeImg = document.querySelector("input[name='imgUrl']");
+const topText = document.querySelector("#topText");
+const bottomText = document.querySelector("#bottomText");
+const addMeme = document.querySelector("#addMeme");
+
+
+function addImage(fileName) {
+    const img = new Image(300, 300);
+    img.src = fileName;
+
+    let textTop = document.createElement("div");
+    let imgDiv = document.createElement("div");
+    let textBottom = document.createElement(("div"))
+
+    imgDiv.classList = "divImage";
+    textTop.classList = "divText";
+    textBottom.classList = "divTextBt";
+
+    textTop.innerHTML = `<span>${topText.value}<\span>`;
+    textBottom.innerHTML = `<span>${bottomText.value}<\span>`;
+    
+    imgDiv.appendChild(img);
+    imgDiv.appendChild(textTop);
+    imgDiv.appendChild(textBottom);
+
+    imgDiv.addEventListener("click", function(e) {
+        if (e.target.tagName === "IMG"){
+            e.target.parentElement.remove();
+        }
+
+    })
+
+    return imgDiv;
+}
+
+
+const imgList = addMeme.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const newImg = addImage(memeImg.value);
+    document.body.appendChild(newImg);
+});
